e309d97b87a543e68a7334dbb5c6003451402d32
Ahmed Abdullah Sourav <aa.shourav@Ahmeds-MacBook-Air.local>
2025-03-10 21:03:38 +0600

    v1.0.0

--- server.js
+++ server.js
@@ -1,42 +1,54 @@
 import express from 'express';
 import path from 'path';
 import { fileURLToPath } from 'url';
-import cors from 'cors';
+
 import auth from './routes/authRoute.js';
 import team from './routes/teamRoute.js';
 import client from './routes/clientRoute.js';
 import lead from './routes/leadRoute.js';
 import docs from './routes/docsRoute.js';
+
 import logger from './middleware/loggerMiddleware.js';
 import errorHandler from './middleware/errorMiddleware.js';
 import notFoundHandler from './middleware/notFoundMiddleware.js';
+
 import connectDB from './db.js';
-import { createServer } from "@vercel/node";
+
+import dotenv from "dotenv";
+dotenv.config();
 
 // Get the directory path of the current module
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+const PORT = process.env.PORT || 3000;
 const app = express();
-app.use(cors());
+
+// body parser middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+// logger middleware
 app.use(logger);
+
+// static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
-// API Routes
+// routes
 app.use('/api/auth', auth);
 app.use('/api/team', team);
 app.use('/api/client', client);
 app.use('/api/lead', lead);
 app.use('/api/docs', docs);
 
-// Database Connection
+// connect to database
 connectDB();
 
-// Error Handling
+// error handler middleware
 app.use(notFoundHandler);
 app.use(errorHandler);
 
-// Export as a handler for Vercel
-export default createServer(app);
+// app.listen(PORT, () => {
+//   console.log(`Server is running on port ${PORT}`);
+// });
+export default app;
