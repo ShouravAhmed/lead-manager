c4f62679232ef28dade37bc0b803af8ed6863d7d
Ahmed Abdullah Sourav <aa.shourav@Ahmeds-MacBook-Air.local>
2025-04-04 21:53:08 +0600

    commit

--- repositories/teamRepository.js
+++ repositories/teamRepository.js
@@ -2,13 +2,13 @@ import Team from '../models/team.js';
 
 class TeamRepository {
   async getTeams() {
-    return await Team.find();
+    return await Team.find().populate('members');
   }
   async getTeamByProperties(properties) {
-    return await Team.find(properties);
+    return await Team.find(properties).populate('members');
   }
   async getTeamById(id) {
-    return await Team.findById(id);
+    return await Team.findById(id).populate('members');
   }
   async createTeam(team) {
     return await Team.create(team);
